1c68dc21fa788e46854fe32449e467a6fc4876d6
Teemu Kokkonen <teemu@i-h.dev>
2024-08-03 20:36:45 +0300

    Spacemeter, music

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 public class SoundManager : MonoBehaviour
 {
+    [SerializeField] private float changeDuration = 1.0f;
+    private float changeStart = -1;
     public AudioSource backgroundMusic1;
     public AudioSource backgroundMusic2;
     private bool isPlayingMusic1;
@@ -14,38 +17,51 @@ public class SoundManager : MonoBehaviour
         // Alkuarvot: soitetaan ensimmäistä taustamusiikkia
         isPlayingMusic1 = true;
         UpdateMusicVolumes();
+        OtherSideManager.WorldChanged += OnWorldChanged;
+    }
+    private void OnDestroy(){
+        
+        OtherSideManager.WorldChanged -= OnWorldChanged;
+    }
+
+    
+
+    private void OnWorldChanged(OtherSideManager.World world)
+    {
+        ToggleMusic(world == OtherSideManager.World.Normal);
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
-            ToggleMusic();
+            ToggleMusic(!isPlayingMusic1);
+        }
+        if(backgroundMusic1.volume != (isPlayingMusic1 ? 1f : 0f)){
+            UpdateMusicVolumes();
         }
     }
 
     void UpdateMusicVolumes()
     {
-      
-    
-
-
-
+        float t = Mathf.Clamp01((Time.unscaledTime - changeStart) / changeDuration);
+        Debug.Log(t);
         if (isPlayingMusic1)
         {
-            backgroundMusic1.volume = 1.0f;
-            backgroundMusic2.volume = 0.0f;
+            backgroundMusic1.volume = t;
+            backgroundMusic2.volume = 1f-t;
         }
         else
         {
-            backgroundMusic1.volume = 0.0f;
-            backgroundMusic2.volume = 1.0f;
+            backgroundMusic1.volume = 1f-t;
+            backgroundMusic2.volume = t;
         }
     }
 
-    public void ToggleMusic()
+    public void ToggleMusic(bool music1)
     {
-        isPlayingMusic1 = !isPlayingMusic1;
+        isPlayingMusic1 = music1;
+        changeStart = Time.unscaledTime;
         UpdateMusicVolumes();
     }
 }
